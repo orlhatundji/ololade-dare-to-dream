3486a057a50e06a32ffbed66b1549710bd728bac
Simon Babatunde <orlhatundji@gmail.com>
2024-07-12 13:54:55 +0100

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/favicon.png",
       },
     },
     "gatsby-plugin-mdx",
